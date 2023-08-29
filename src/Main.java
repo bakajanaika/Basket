589cc05328d8a9cb285cecde94a48640550c15a9
Morgan <proger20000@gmail.com>
2023-08-29 10:41:56 +0600

    ...

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Yan ya tut");
+        System.out.println("Yan ya tut");
+        System.out.println("Yan ya tut");
+        System.out.println("Yan ya tut");
+        System.out.println("Yan ya tut");
 //dev
 
         //comment1111111
