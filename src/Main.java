2b2192efee0a8b17d038f5fb00dcc80045d174a7
Ian <zxc@zxc.com>
2023-08-29 10:52:14 +0600

    cls commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Yan ya tut");
-        System.out.println("Yan ya tut");
-        System.out.println("Yan ya tut");
-        System.out.println("Yan ya tut");
-        System.out.println("Yan ya tut");//
-//dev
-
-        //comment1111111
-    }
-    // ya tut Yan
 
 
 }
